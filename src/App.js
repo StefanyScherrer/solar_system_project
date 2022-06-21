58bd93a9346e8da39aa7a07f8b8d38032c0a05d8
Stefany Scherrer <devstefanyscherrer@gmail.com>
2022-06-21 17:32:01 -0300

    Requisito 1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import Header from './components/header';
 
 class App extends React.Component {
   render() {
-    return (<p>Sistema Solar</p>);
+    return (
+      <Header />
+    );
   }
 }
 
 export default App;
+
+// COMO É ORIGINALMENTE:
+// import React from 'react';
+
+// class App extends React.Component {
+//   render() {
+//     return (<p>Sistema Solar</p>);
+//   }
+// }
+
+// export default App;
